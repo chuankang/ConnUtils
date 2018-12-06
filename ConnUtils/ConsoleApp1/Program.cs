b814d24f2175b4aee65549f8ae341c3b04dfb1c9
cksun <1037134100@qq.com>
2018-12-06 17:47:22 +0800

    引用测试包

--- ConnUtils/ConsoleApp1/Program.cs
+++ ConnUtils/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ConnUtils;
 
 namespace ConsoleApp1
 {
@@ -7,30 +8,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var studentList = new List<Student>
-            {
-                new Student {Name = "吴明军", Age = 40},
-                new Student {Name = "孙传康", Age = 20},
-            };
-
-            foreach (var student in studentList)
-            {
-                if (student.Name == "吴明军")
-                {
-                    student.Name = "张恒";
-                }
-            }
-
-            Console.WriteLine($"{studentList[0].Name},{studentList[1].Name}");
-
-            Console.ReadLine();
+            var list = new List<int>();
+            Utils.ListToTable(list);
+            Console.WriteLine();
         }
     }
-
-    public class Student
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-
 }
