0982dbcb3cbde61bdf0cafac5148d55ea39ceb6b
cksun <1037134100@qq.com>
2018-12-06 17:35:39 +0800

    version1

---
+++ ConnUtils/ConnUtils/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace ConnUtils
+{
+    public class Utils
+    {
+        /// <summary>
+        /// List转DataTable
+        /// </summary>
+        public static DataTable ListToTable<T>(IList<T> list)
+        {
+            var type = typeof(T);
+            var dt = new DataTable();
+            var propertyInfoList = type.GetProperties(BindingFlags.DeclaredOnly 
+                                                      | BindingFlags.Public | BindingFlags.Instance);
+            foreach (var property in propertyInfoList)
+            {
+                dt.Columns.Add(property.Name);
+            }
+            foreach (var model in list)
+            {
+                var row = dt.NewRow();
+                dt.Rows.Add(row);
+                foreach (var property in propertyInfoList)
+                {
+                    row[property.Name] = property.GetValue(model, null);
+                }
+            }
+            
+            return dt;
+        }
+    }
+}
---
+++ ConnUtils/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentList = new List<Student>
+            {
+                new Student {Name = "吴明军", Age = 40},
+                new Student {Name = "孙传康", Age = 20},
+            };
+
+            foreach (var student in studentList)
+            {
+                if (student.Name == "吴明军")
+                {
+                    student.Name = "张恒";
+                }
+            }
+
+            Console.WriteLine($"{studentList[0].Name},{studentList[1].Name}");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+}
