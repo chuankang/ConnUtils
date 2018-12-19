90ed9d8e6de67df139e253d1efc15002175b1b66
cksun <1037134100@qq.com>
2018-12-19 16:59:54 +0800

    StringHelper

--- ConnUtils/ConnUtils/Utils.cs
+++ ConnUtils/ConnUtils/Utils.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Reflection;
+using System.Text;
 
 namespace ConnUtils
 {
@@ -30,6 +32,79 @@ namespace ConnUtils
             }
             
             return dt;
+        }
+
+        /// <summary>
+        /// 时间戳转时间
+        /// </summary>
+        public DateTime StampToDateTime(string timeStamp)
+        {
+            DateTime dateTimeStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long lTime = long.Parse(timeStamp + "0000000");
+            TimeSpan toNow = new TimeSpan(lTime);
+
+            return dateTimeStart.Add(toNow);
+        }
+
+        /// <summary>
+        /// DateTime时间格式转换为Unix时间戳格式
+        /// </summary>
+        public int DateTimeToStamp(DateTime time)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            return (int)(time - startTime).TotalSeconds;
+        }
+
+        /// <summary>
+        /// Base64解密，采用utf8编码方式解密
+        /// </summary>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string DecodeBase64(string result)
+        {
+            return DecodeBase64(Encoding.UTF8, result);
+        }
+
+        /// <summary>
+        /// Base64解密
+        /// </summary>
+        /// <param name="encode">解密采用的编码方式，注意和加密时采用的方式一致</param>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string DecodeBase64(Encoding encode, string result)
+        {
+            string decode;
+            var bytes = Convert.FromBase64String(result);
+            try
+            {
+                decode = encode.GetString(bytes);
+            }
+            catch
+            {
+                decode = result;
+            }
+            return decode;
+        }
+
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="encodeType">加密采用的编码方式</param>
+        /// <param name="source">待加密的明文</param>
+        /// <returns></returns>
+        public static string Base64Encode(Encoding encodeType, string source)
+        {
+            string encode;
+            byte[] bytes = encodeType.GetBytes(source);
+            try
+            {
+                encode = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encode = source;
+            }
+            return encode;
         }
     }
 }
